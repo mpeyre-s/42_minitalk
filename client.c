04c5b7df116395e16ac979140db0e28b3d6f3cec
mpeyre-s <mathispeyre0@gmail.com>
2024-12-27 19:11:02 +0100

    fix segfault ft_printf

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: mathispeyre <mathispeyre@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 15:39:07 by mathispeyre       #+#    #+#             */
-/*   Updated: 2024/12/27 18:00:35 by mathispeyre      ###   ########.fr       */
+/*   Updated: 2024/12/27 19:10:31 by mathispeyre      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 int	outbox(int server_pid, char *str)
 {
-	ft_printf("Server PID : %d, Message : %s\n", server_pid, str);
+	ft_printf("Server PID : %d\n", server_pid);
+	ft_printf("Message : %s\n", str);
 	ft_printf("Abort... Not internet connection :/");
 	return (0);
 }
 
-int	main(int ac, char *av[])
+int	main(int ac, char **av)
 {
 	if (ac != 3)
 		return (ft_printf("Wrong nb of args"));
